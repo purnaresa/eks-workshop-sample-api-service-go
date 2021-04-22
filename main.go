13a1b876f564e8c027c836ebd59d57dcb79f2479
Purnaresa <purnaresa@gmail.com>
2021-04-22 14:39:29 +0700

    Revert "Purnaresa patch rekog"

--- main.go
+++ main.go
@@ -1,154 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
-	"mime/multipart"
+	"io"
 	"net/http"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/rekognition"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
+	"os"
+	"sort"
+	"strings"
 )
 
-var svc *rekognition.Rekognition
-var svcS3 *s3.S3
-
-func init() {
-
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("ap-southeast-1"),
-	})
-
-	if err != nil {
-		log.Fatalln("Error while creating session,", err)
-		return
-	}
-
-	svc = rekognition.New(sess)
-	svcS3 = s3.New(sess)
-}
-
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-	router := gin.Default()
-	router.Use(cors.Default())
+		f := fib()
 
-	router.GET("/", healthcheck)
-	router.POST("/compare", ComparesFace)
-	router.Run(":8080")
-}
+		res := &response{Message: "Hello Post-Apocalypse World"}
 
-func healthcheck(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Welcome to eKYC Demo 0.1"})
-}
-
-func ComparesFace(c *gin.Context) {
-
-	fileSource, _ := c.FormFile("fileSource")
-	fileTarget, _ := c.FormFile("fileTarget")
-
-	fileSourceNameData, err := fileSource.Open()
-	if err != nil {
-		log.Println(err)
-	}
-	fileSourceName, errUpload := UploadFile(fileSource.Filename, fileSourceNameData)
-	if err != nil {
-		log.Println(errUpload)
-	}
-
-	fileTargetData, err := fileTarget.Open()
-	if err != nil {
-		log.Println(err)
-	}
-	fileTargetName, errUpload := UploadFile(fileTarget.Filename, fileTargetData)
-	if err != nil {
-		log.Println(errUpload)
-	}
-
-	inputCompare := &rekognition.CompareFacesInput{
-		SimilarityThreshold: aws.Float64(90.000000),
-		SourceImage: &rekognition.Image{
-			S3Object: &rekognition.S3Object{
-				Bucket: aws.String("kyc-test-11571204"),
-				Name:   aws.String(fileSourceName),
-			},
-		},
-		TargetImage: &rekognition.Image{
-			S3Object: &rekognition.S3Object{
-				Bucket: aws.String("kyc-test-11571204"),
-				Name:   aws.String(fileTargetName),
-			},
-		},
-	}
-
-	result, err := svc.CompareFaces(inputCompare)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case rekognition.ErrCodeInvalidParameterException:
-				log.Println(rekognition.ErrCodeInvalidParameterException, aerr.Error())
-			case rekognition.ErrCodeInvalidS3ObjectException:
-				log.Println(rekognition.ErrCodeInvalidS3ObjectException, aerr.Error())
-			case rekognition.ErrCodeImageTooLargeException:
-				log.Println(rekognition.ErrCodeImageTooLargeException, aerr.Error())
-			case rekognition.ErrCodeAccessDeniedException:
-				log.Println(rekognition.ErrCodeAccessDeniedException, aerr.Error())
-			case rekognition.ErrCodeInternalServerError:
-				log.Println(rekognition.ErrCodeInternalServerError, aerr.Error())
-			case rekognition.ErrCodeThrottlingException:
-				log.Println(rekognition.ErrCodeThrottlingException, aerr.Error())
-			case rekognition.ErrCodeProvisionedThroughputExceededException:
-				log.Println(rekognition.ErrCodeProvisionedThroughputExceededException, aerr.Error())
-			case rekognition.ErrCodeInvalidImageFormatException:
-				log.Println(rekognition.ErrCodeInvalidImageFormatException, aerr.Error())
-			default:
-				log.Println(aerr.Error())
-			}
-			c.JSON(http.StatusBadRequest, err.Error())
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			log.Println(err.Error())
-			c.JSON(http.StatusInternalServerError, err.Error())
+		for _, e := range os.Environ() {
+			pair := strings.Split(e, "=")
+			res.EnvVars = append(res.EnvVars, pair[0]+"="+pair[1])
 		}
-		return
-	}
+		sort.Strings(res.EnvVars)
 
-	c.JSON(http.StatusOK, result)
+		for i := 1; i <= 90; i++ {
+			res.Fib = append(res.Fib, f())
+		}
+
+		// Beautify the JSON output
+		out, _ := json.MarshalIndent(res, "", "  ")
+
+		// Normally this would be application/json, but we don't want to prompt downloads
+		w.Header().Set("Content-Type", "text/plain")
+
+		io.WriteString(w, string(out))
+
+		fmt.Println("Hello world - the log message")
+	})
+	
+	
+	
+	http.ListenAndServe(":8080", nil)
 }
 
-func UploadFile(fileName string, file multipart.File) (newFileName string, err error) {
-	guid := xid.New()
+type response struct {
+	Message string   `json:"message"`
+	EnvVars []string `json:"env"`
+	Fib     []int    `json:"fib"`
+}
 
-	newFileName = fmt.Sprintf("%s-%s", guid.String(), fileName)
-
-	input := &s3.PutObjectInput{
-		Body:   file,
-		Bucket: aws.String("kyc-test-11571204"),
-		Key:    aws.String(newFileName),
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
 	}
-
-	_, err = svcS3.PutObject(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				log.Println(aerr.Error())
-			}
-		} else {
-			log.Println(err.Error())
-		}
-		return
-	}
-
-	return
-
 }
